6705a5dc4092a27878af7a90b92c9e40e0b8643b
shorner102 <sydnihorner@gmail.com>
2018-01-30 16:43:30 -0500

    New routes

--- PainAppDatabaseAccess/src/org/pain/app/controller/SyndromeController.java
+++ PainAppDatabaseAccess/src/org/pain/app/controller/SyndromeController.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -17,6 +18,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBElement;
 
 import org.pain.app.bean.Syndrome;
 import org.pain.app.service.SyndromeService;
@@ -30,17 +33,41 @@ public class SyndromeController {
 
 	
 	//Change this to post
-	@GET
+	//Id will come in json
+/*	@GET
 	@Path("/sBodyRegion/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List getSyndromesBySpecificBodyRegionId(@PathParam("id") int id) throws SQLException {
 		List listOfSyndromes = syndromeService.getAllSyndromesInSpecificBodyRegion(id);
 		return listOfSyndromes;
-	}
+	}*/
 	
-	/*
-	 * 
-	 */
+	
+	//He gives a body region id, and he needs back specific body region ids and name for one 
+	//Another he gives a specific body region id, and he needs back syndromes
+	//given a syndrome, get all the information back for it
+	
+	
+	@POST
+	@Path("/sBodyRegion/")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getSyndromesBySpecificBodyRegionId(@PathParam("sBodyRegionId") int sBodyRegionId) throws SQLException {
+		List listOfSyndromes = syndromeService.getAllSyndromesInSpecificBodyRegion(sBodyRegionId);
+		//return listOfSyndromes;
+		return Response.ok(listOfSyndromes).build();
+	}
+
+
+	@POST
+	@Path("/bodyRegion/")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getSyndromesByBodyRegionId(@PathParam("bodyRegionId") int bodyRegionId) throws SQLException {
+		List listOfSyndromes = syndromeService.getAllSyndromesInBodyRegion(bodyRegionId);
+		//return listOfSyndromes;
+		return Response.ok(listOfSyndromes).build();
+	}
 	
 	
 	@GET
--- PainAppDatabaseAccess/src/org/pain/app/service/SyndromeService.java
+++ PainAppDatabaseAccess/src/org/pain/app/service/SyndromeService.java
@@ -20,37 +20,48 @@ public class SyndromeService {
 		if (syndromeIdMap == null) {
 			syndromeIdMap = new HashMap<Integer, Syndrome>();
 			// Creating some objects of Syndrome while initializing
-			BodyRegion hip = new BodyRegion(1, "Hip");
-			BodyRegion forearm = new BodyRegion(2, "Forearm");
-			BodyRegion foot = new BodyRegion(3, "Foot");
+			BodyRegion hip = new BodyRegion(0, "Hip");
+			BodyRegion forearm = new BodyRegion(1, "Forearm");
+			BodyRegion foot = new BodyRegion(2, "Foot");
 			
-			SpecificBodyRegion medial = new SpecificBodyRegion(1, "Medial", hip);
-			SpecificBodyRegion posterior = new SpecificBodyRegion(2, "Posterior", hip);
-			SpecificBodyRegion medial2 = new SpecificBodyRegion(3, "Medial", forearm);
-			SpecificBodyRegion lateral = new SpecificBodyRegion(4, "Lateral", foot);
+			SpecificBodyRegion medial = new SpecificBodyRegion(0, "Medial", hip);
+			SpecificBodyRegion posterior = new SpecificBodyRegion(1, "Posterior", hip);
+			SpecificBodyRegion medial2 = new SpecificBodyRegion(2, "Medial", forearm);
+			SpecificBodyRegion lateral = new SpecificBodyRegion(3, "Lateral", foot);
 			
 					
-			Syndrome ovarianCyst = new Syndrome(1, "Ovarian Cyst", hip, medial);
-			Syndrome endometriosis = new Syndrome(2, "Endometriosis", hip, medial);
-			Syndrome sacralStressFX = new Syndrome(3, "Sacral Stress FX", hip, posterior);
-			Syndrome ulnarBursitis = new Syndrome(4, "Ulnar Bursitis", forearm, medial2);
-			Syndrome highAnkleSprain = new Syndrome(5, "High Ankle Sprain", foot, lateral);
+			Syndrome ovarianCyst = new Syndrome(0, "Ovarian Cyst", hip, medial);
+			Syndrome endometriosis = new Syndrome(1, "Endometriosis", hip, medial);
+			Syndrome sacralStressFX = new Syndrome(2, "Sacral Stress FX", hip, posterior);
+			Syndrome ulnarBursitis = new Syndrome(3, "Ulnar Bursitis", forearm, medial2);
+			Syndrome highAnkleSprain = new Syndrome(4, "High Ankle Sprain", foot, lateral);
 
-			syndromeIdMap.put(1, ovarianCyst);
-			syndromeIdMap.put(2, endometriosis);
-			syndromeIdMap.put(3, sacralStressFX);
-			syndromeIdMap.put(4, ulnarBursitis);
-			syndromeIdMap.put(5, highAnkleSprain);
+			syndromeIdMap.put(0, ovarianCyst);
+			syndromeIdMap.put(1, endometriosis);
+			syndromeIdMap.put(2, sacralStressFX);
+			syndromeIdMap.put(3, ulnarBursitis);
+			syndromeIdMap.put(4, highAnkleSprain);
 		}
 	}
-	public List getAllSyndromesInSpecificBodyRegion(int sBodyRegionId) {
+	public List getAllSyndromesInBodyRegion(int bodyRegionId) {
 		List syndromes = new ArrayList();
-		for(int i = 1; i <= syndromeIdMap.size(); i++) {
-			if(syndromeIdMap.get(i).getsBodyRegion().getId() == sBodyRegionId) {
+		for(int i = 0; i < syndromeIdMap.size(); i++) {
+			if(syndromeIdMap.get(i).getBodyRegion().getId() == bodyRegionId) {
 				syndromes.add(syndromeIdMap.get(i));
 			}
 		}
 		return syndromes;
+	}
+	
+	
+	public List getAllSyndromesInSpecificBodyRegion(int sBodyRegionId) {
+		List sBodyRegions = new ArrayList();
+		for(int i = 0; i < syndromeIdMap.size(); i++) {
+			if(syndromeIdMap.get(i).getsBodyRegion().getId() == sBodyRegionId) {
+				sBodyRegions.add(syndromeIdMap.get(i));
+			}
+		}
+		return sBodyRegions;
 	}
 	
 	public List getAllSyndromes() {
