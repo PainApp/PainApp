a1aae7a6978c45a3f437b2d4266c1e80aded8fee
Javier Serrano <jeserrano117@gmail.com>
2018-04-11 19:02:31 -0400

    updated to run on ec2 server as service

--- node/connection/mysqlConnection.js
+++ node/connection/mysqlConnection.js
@@ -1,16 +1,6 @@
 const mysql = require("mysql2/promise");
-const prompt = require("prompt-sync")();
+const config = require("./config.json")
 
-var pass = prompt.hide("mysql password: ");
-
-pool = mysql.createPool({
-  host               : "pocketdoc.cxx8xktili9j.us-east-2.rds.amazonaws.com",
-  user               : "ghost",
-  password           : pass,
-  port               : "3306",
-  database           : "pocket_doc",
-  multipleStatements : true,
-  connectionLimit    : 100
-});
+pool = mysql.createPool(config);
 
 exports.pool = pool;
